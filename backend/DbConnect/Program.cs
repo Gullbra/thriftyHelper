a014042ab788dcf6942a3d2a1c12fd68fbf2989c
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-01-31 15:53:27 +0100

    learning how to create a table

--- backend/DbConnect/Program.cs
+++ backend/DbConnect/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Npgsql;
 using System;
+using System.Data.SqlTypes;
 using ThriftyHelper.Backend.DbConnect;
 
 Sectrets ENV = new();
@@ -9,8 +10,40 @@ using NpgsqlConnection dbConnection = new(connectionString);
 
 dbConnection.Open();
 
+/* Access Scalar
 string sqlString = "SELECT version()";
 using NpgsqlCommand sqlCommand = new (sqlString, dbConnection);
-
+ 
 string sqlVersion = sqlCommand.ExecuteScalar().ToString();
 Console.WriteLine($"PostgreSQL version: {sqlVersion}");
+ */
+
+/* Create Table*/
+using NpgsqlCommand sqlCommand = new();
+sqlCommand.Connection = dbConnection;
+
+sqlCommand.CommandText = "DROP TABLE IF EXISTS cars";
+sqlCommand.ExecuteNonQuery();
+
+sqlCommand.CommandText = @"CREATE TABLE cars(id SERIAL PRIMARY KEY,
+	name VARCHAR(255), price INT)";
+sqlCommand.ExecuteNonQuery();
+
+List<KeyValuePair<string, int>> carList = new ()
+{
+	new KeyValuePair<string, int>("Audi", 52642),
+	new KeyValuePair<string, int>("Mercedes", 57127),
+	new KeyValuePair<string, int>("Skoda", 9000),
+	new KeyValuePair<string, int>("Volve", 29000),
+	new KeyValuePair<string, int>("Bently",350000),
+	new KeyValuePair<string, int>("Citroën", 21000),
+	new KeyValuePair<string, int>("Volkswagen", 21600)
+};
+
+foreach (var kv in carList)
+{
+	sqlCommand.CommandText = $"INSERT INTO cars(name, price) VALUES ('{kv.Key}', {kv.Value})";
+	sqlCommand.ExecuteNonQuery();
+}
+
+Console.WriteLine("Table cars created");
