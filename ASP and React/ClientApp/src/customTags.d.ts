cbf37507afbd861c7eb0dfd626b329e700d263c4
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-01-31 14:11:36 +0100

    Removing templates, work on dbConnection

--- ASP and React/ClientApp/src/customTags.d.ts
+++
@@ -1,9 +0,0 @@
-export as namespace JSX;
-
-export interface IntrinsicElements {
-  "header-column": any;
-  "loading-spinner": any;
-  "grid-wrapper": any;
-  "list-wrapper": any;
-  "form-wrapper": any;
-}
---
+++ backend/DbConnect/Program.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+using System;
+using ThriftyHelper.Backend.DbConnect;
+
+Sectrets ENV = new();
+
+string connectionString = $"Host={ENV.Host};Username={ENV.Username};Password={ENV.Password};Database={ENV.Database}";
+using NpgsqlConnection dbConnection = new(connectionString);
+
+dbConnection.Open();
+
+string sqlString = "SELECT version()";
+using NpgsqlCommand sqlCommand = new (sqlString, dbConnection);
+
+string sqlVersion = sqlCommand.ExecuteScalar().ToString();
+Console.WriteLine($"PostgreSQL version: {sqlVersion}");
